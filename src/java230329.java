ab0bcf6c7809ef269ae000983fa626e282d7dd99
SUN <qiosion@gmail.com>
2023-04-05 10:38:28 +0900

    스캐너

---
+++ src/java230329.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class java230329 {
+    public static void main(String[] args) {
+        // 스캐너
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.next();
+        String city = scanner.next();
+
+        int age = scanner.nextInt();
+        double weight = scanner.nextDouble();
+        boolean isSingle = scanner.nextBoolean();
+
+        System.out.println(name);
+        System.out.println(city);
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(isSingle);
+    }
+}
