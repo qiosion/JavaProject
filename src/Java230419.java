b035ac6938e324e13e03d557e578d5469fec3a30
SUN <qiosion@gmail.com>
2023-04-19 11:42:56 +0900

    메서드에서 배열 리턴

---
+++ src/Java230419.java
@@ -0,0 +1,17 @@
+public class Java230419 {
+    public static void main(String[] args) {
+        int intArray[];
+        intArray = makeArray();
+        for (int i = 0; i < 4; i++) {
+            System.out.print(intArray[i] + " ");
+        }
+    }
+
+    private static int[] makeArray() {
+        int temp[] = new int[4];
+        for (int i = 0; i < 4; i++) {
+            temp[i] = i + 1;
+        }
+        return temp;
+    }
+}
