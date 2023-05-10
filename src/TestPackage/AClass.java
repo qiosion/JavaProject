b477ba612125b1f069d09f179c0d5a24be96829c
SUN <qiosion@gmail.com>
2023-05-10 10:46:32 +0900

    패키지 간단 설명

---
+++ src/TestPackage/AClass.java
@@ -0,0 +1,11 @@
+package TestPackage;
+
+import TestPackage2.BClass;
+
+public class AClass {
+    public static void main(String[] args) {
+        BClass b = new BClass();
+        b.i = 10;
+        b.test();
+    }
+}
---
+++ src/TestPackage2/BClass.java
@@ -0,0 +1,9 @@
+package TestPackage2;
+
+public class BClass {
+    public int i;
+    public void test() {
+        System.out.println("i 는 " + i);
+        System.out.println("이건 TestPackage2의 test 메서드");
+    }
+}
