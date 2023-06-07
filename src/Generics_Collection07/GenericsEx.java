36e0ad7e350918059ac2c2447dfc8bad3bf2d713
SUN <qiosion@gmail.com>
2023-06-07 11:56:43 +0900

    제네릭

---
+++ src/Generics_Collection07/GenericsEx.java
@@ -0,0 +1,30 @@
+package Generics_Collection07;
+
+class GenClass<T> {
+    T val;
+    void set(T t) {
+        val = t;
+    }
+    T get() {
+        return val;
+    }
+}
+
+public class GenericsEx {
+    public static void main(String[] args) {
+        // GenClass의 T를 String으로 지정하겠다
+        GenClass<String> strGen = new GenClass<String>();
+        strGen.set("Hello");
+        System.out.println("strGen : " + strGen.get()); // Hello
+
+        // GenClass의 T를 Integer로 지정하겠다
+        GenClass<Integer> intGen = new GenClass<>();
+        intGen.set(111);
+        System.out.println("intGen : " + intGen.get()); // 111
+
+        // GenClass의 T를 Integer로 지정하겠다
+        GenClass<Float> floatGen = new GenClass<>();
+        floatGen.set(10.2f);
+        System.out.println("floatGen : " + floatGen.get()); // 10.2
+    }
+}
